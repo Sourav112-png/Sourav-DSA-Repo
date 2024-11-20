2b21adaca0f719b400637dd3d15b01ec7129ad77
Sourav Kumar Meher <74526702+Sourav112-png@users.noreply.github.com>
2024-11-21 00:05:49 +0530

    Update ArraysAndHashing3.java

--- ArraysAndHashing/ArraysAndHashing3.java
+++ ArraysAndHashing/ArraysAndHashing3.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 //1. Two Sum
 public class ArraysAndHashing3 {
 	public static void main(String[] args) {
-		int[] arr = { 3,3 };
+		int[] arr = { 3, 3 };
 		int target = 6;
 		int[] ans = twoSum(arr, target);
 		System.out.println(Arrays.toString(ans));
@@ -13,9 +13,8 @@ public class ArraysAndHashing3 {
 
 	public static int[] twoSum(int[] nums, int target) {
 		int[] ans = new int[2];
-		int sum = 0;
 		for (int i = 0; i < nums.length; i++) {
-			for (int j = i+1; j < nums.length; j++) {
+			for (int j = i + 1; j < nums.length; j++) {
 				if (nums[i] + nums[j] == target) {
 					ans[0] = j;
 					ans[1] = i;
